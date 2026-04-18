6b5b1bcbf666b704484a3c39d8a16a41ad73e538
Stefan1400 <stefan.besonen783@gmail.com>
2026-04-18 16:20:08 -0700

    feat(home-feed): extract feed into reusable component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+import Feed from "./components/Feed";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Link href='/feed'>Feed</Link>
+      <Feed />
     </div>
   );
 }
