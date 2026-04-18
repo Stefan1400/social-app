f827e727bad47fe7f362843178e6df857e70fe0a
Stefan1400 <stefan.besonen783@gmail.com>
2026-04-18 16:51:45 -0700

    feat(home-feed): automatically fetch and display posts

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -1,40 +1,24 @@
 'use client';
-import { useState } from "react";
 import Post from "../components/Post";
 import type { PostTypes } from "../components/Post";
 import Link from "next/link";
 
-export default function Feed() {
-   const [postData, setPostData] = useState<PostTypes[] | null>(null);
-   
-   async function handleGetPosts() {
-      
-      const res = await fetch('/api/posts/get-posts', {
-         method: 'GET'
-      })
+type FeedTypes = {
+   posts: PostTypes[];
+};
 
-      if (!res.ok) {
-         console.log('response was not okay:', res);
-      };
-
-      const result = await res.json();
-
-      setPostData(result.posts);
-   }
-   
+export default function Feed({ posts }: FeedTypes) {
    return (
       <div className="w-screen h-screen p-3 flex flex-col items-center">
          <div className="fixed left-0 top-15 p-5 z-100 w-screen flex justify-between px-3">
             <h1>Feed</h1>
-
-            <button onClick={handleGetPosts}>Get Posts</button>
          </div>
          
          <ul className="list-none mt-30 flex flex-col w-screen p-5 gap-5">
-            {postData?.map((p, index) => {
+            {posts.map((p, index) => {
 
             return (
-               <li key={index}>
+               <li key={p.id}>
                   <Link href={`/pages/view-post/${p.id}`} >
                      <Post 
                         id={p.id}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { prisma } from "@/lib/prisma";
 import Feed from "./components/Feed";
 
-export default function Home() {
+export default async function Home() {
+  
+  const fetchedPosts = await prisma.post.findMany({
+    include: {
+      user: true
+    }
+  })
+  
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Feed />
+      <Feed posts={fetchedPosts} />
     </div>
   );
 }
